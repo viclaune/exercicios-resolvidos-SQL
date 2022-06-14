77a785e719f7bcacbec4dd01c0f5b84e2d596c7c
Victor Launé <victorh.laune@gmail.com>
2022-06-14 09:18:31 -0300

    Solution Sation 6 and 7

---
+++ 14-06-22 - 12 Weather Observation Station 7.sql
@@ -0,0 +1,19 @@
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+SELECT  DISTINCT(CITY)
+  FROM  STATION
+ WHERE  CITY LIKE '%a' 
+        OR CITY LIKE '%e'
+        OR CITY LIKE '%i'
+        OR CITY LIKE '%o'
+        OR CITY LIKE '%u';
