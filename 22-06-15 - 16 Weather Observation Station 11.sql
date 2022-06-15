812784b0ce6ef22330175afc56ee44fb1e67a709
Victor Launé <victorh.laune@gmail.com>
2022-06-15 09:50:20 -0300

    Solved Weather Observation Station 11 and 12

---
+++ 22-06-15 - 16 Weather Observation Station 11.sql
@@ -0,0 +1,25 @@
+/*
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+SELECT  DISTINCT CITY
+  FROM  STATION
+ WHERE  NOT (CITY LIKE 'a%'
+          OR CITY LIKE 'e%'
+          OR CITY LIKE 'i%'
+          OR CITY LIKE 'o%'
+          OR CITY LIKE 'u%'
+        ) OR NOT (
+          CITY LIKE '%a'
+          OR CITY LIKE '%e'
+          OR CITY LIKE '%i'
+          OR CITY LIKE '%o'
+          OR CITY LIKE '%u');
