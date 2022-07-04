673a4c3d25f827dce1ebc8f882f943dfe221fdc9
Victor Laune <victorh.laune@gmail.com>
2022-07-04 00:53:25 -0300

    Reorganizacao da pasta.

---
+++ HackerRank/22-07-04 - 34 Weather Observation Station 17.sql
@@ -0,0 +1,16 @@
+/*
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780. Round your answer to 4 decimal places.
+
+Input Format
+
+The STATION table is described as follows:
+https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE LAT_N = (SELECT MIN(LAT_N)
+                 FROM STATION
+                WHERE LAT_N > 38.7780)
